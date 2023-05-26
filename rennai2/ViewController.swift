40ead270a68b316f09b6f997ff391e3271d17408
Takai Yuzuki <youyuegaojing 13@gmail.com>
2023-05-26 18:22:32 +0900

    Initial Commit

---
+++ rennai2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rennai2
+//
+//  Created by 高井佑月 on 2023/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
